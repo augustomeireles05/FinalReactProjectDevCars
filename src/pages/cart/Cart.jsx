7ec4a185c2ef59bc8188e2c53d390a46564d1ddf
WThiersLucena <you@example.com>
2022-05-23 23:35:12 -0300

    CART (carrinho de compra ) - finalizado

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -5,17 +5,10 @@ import { Link } from 'react-router-dom'
 import Button from '../../components/Button/Button'
 import Sidebar from '../../components/Sidebar/Sidebar'
 import Subtittle from '../../components/Subtittle/Subtittle'
-
-
-
-// import imgCarrinho from '../../assets/images/Cart/bmw-x5.jpg'
 import React, { useEffect, useContext } from 'react'
-
 import MiniCart from '../../components/Cart/MiniCart'
-
 import CartContext from '../../contexts/cart.provider'
 import axios from 'axios'
-
 import { baseUrl } from '../../environments'
 import { useState } from 'react'
 
@@ -23,147 +16,142 @@ import { useState } from 'react'
 
 
 function Cart() {
-
-
-    const URL = `${baseUrl}/cardData`
-    const { getCart, cart } = useContext(CartContext)
+    const URL = `${baseUrl}/cart`
+    const { getCart, cart, setCart, deleteCart } = useContext(CartContext)
+    // const [ cart, setCart ] = useState ([])
 
     useEffect(() => {
         getCart()
     }, [])
-    
-
-    // const deleteCart = (id) => {
-    //     axios.delete(`${URL}/${id}`)
-    //     .then((response) => {
-    //         getCart()
-    //     })
-    // }
-
-    
-
-    const apagar = (cart) => {
-            localStorage.clear(cart)
-        }
-        const exibir = () => {
-            let novo = "Limpo";
-            for (let i = 0, len = localStorage.length; i < len; i++) {
-                let value = localStorage.getItem("cart")
-            }
-        }
 
 
+    function CardProduct() {
         return (
-            <>
 
-                <Header />
+            <div className="row col-11 col-md-11 col-lg-12 col-xl-12 align-items-center bg-body rounded-3 shadow justify-content-center py-lg-3 m-auto">
+                <div className="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 pt-2 pt-md-0 pt-lg-0 pt-2 pt-md-2 pt-lg-0 pt-xl-0">
+                    <img src={cart[10]} className="img-fluid" />
+                </div>
+                <div className="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4">
 
-                <MiniCart />
+                    <div className="font-cart">
+                        <p className="text-center pt-3 fw-bold m-0">{cart[1]} {cart[2]}</p>
+                    </div>
+                    <div className="">
+                        <p className="texto-alinhado p-2">
+                            {cart[11]}
+                        </p>
+                    </div>
+                </div>
+                <div className="row col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 text-center p-0 mb-3 mb-md-0 mb-lg-0 mb-3 mb-md-4 mb-lg-0">
 
-                {/* BEGIN TITULO PAGINA */}
-
-                {/* BEGIN CONTAINER PRINCIPAL */}
-                <main className="background-grey mt-2 mb-0">
-
-                    <div className="row col-12 col-sm-12 col-md-12 col-lg-11 col-xl-11 m-auto">
-                        {/* Início Container Lado esquerdo */}
-                        <div className="col-12 col-sm-12 col-md-3 col-lg-3 col-xl-3">
-                            {/* Inicio subtitulo lado esquerdo */}
-                            <div className="">
-                                <Subtittle menu="Menu" />
-                            </div>
-                            {/* Fim subtitulo lado esquerdo */}
-
-                            {/* Inicio Sidebar */}
-                            <div className="">
-                                <Sidebar />
-                            </div>
-                            {/* Fim Sidebar */}
-                        </div>
-                        {/* Fim Container Lado esquerdo */}
-
-
-                        {/* Início Container Lado direito */}
-                        <div className="col-12 col-sm-12 col-md-9 col-lg-9 col-xl-9 px-0">
-
-                            {/* Inicio subtitulo lado direito */}
-                            <div className="">
-                                <Subtittle menu="Carrinho de compra" />
-                            </div>
-                            {/* Fim subtitulo lado direito */}
-
-
-
-
-                            {/* Inicio Card lado direito */}
-                            <div className="row col-11 col-md-11 col-lg-12 col-xl-12 align-items-center bg-body rounded-3 shadow justify-content-center py-lg-3 m-auto">
-                                {/* Inicio Div Imagem */}
-                                <div className="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 pt-2 pt-md-0 pt-lg-0 pt-2 pt-md-2 pt-lg-0 pt-xl-0">
-                                    <img src={cart[10]} className="img-fluid" />
-                                </div>
-                                {/* Fim Div Imagem */}
-
-                                {/* Inicio Div Nome + Resumo */}
-                                <div className="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4">
-                                    {/* Inicio Nome */}
-                                    <div className="font-cart">
-                                        <p className="text-center pt-3 fw-bold m-0">{cart[1]} {cart[2]}</p>
-                                    </div>
-                                    {/* Fim Div Nome */}
-
-                                    {/* Inicio Resumo */}
-                                    <div className="">
-                                        <p className="texto-alinhado p-2">
-                                            {cart[11]}
-                                        </p>
-                                    </div>
-                                    {/* Fim Div Resumo */}
-                                </div>
-                                {/* Fim Div Nome + Resumo */}
-
-                                {/* Inicio Div Preco + Lixo */}
-                                <div className="row col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 text-center p-0 mb-3 mb-md-0 mb-lg-0 mb-3 mb-md-4 mb-lg-0">
-                                    {/* Inicio Preço */}
-                                    <div className="col-9 col-sm-9 col-md-9 col-lg-10 col-xl-10">
-                                        <p className="text-center m-0 price-style"> {cart[7]}</p>
-                                    </div>
-                                    {/* Fim Div Preço */}
-
-                                </div>
-                                {/* Fim Div Preco + Lixo */}
-                            </div>
-                            {/* Fim Card lado direito */}
-
-
-                            <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 px-0 mt-3 d-flex justify-content-center d-md-flex justify-content-md-center">
-                                <div className="col-11 col-md-11 col-lg-12 col-xl-12 d-lg-flex justify-content-lg-end">
-                                    <Button link="/checkoutOrder" name="Continuar" />
-                                </div>
-                                {/* Inicio Lixeira */}
-                                <div className="col-3 col-sm-3 col-md-3 col-lg-2 col-xl-2 mx-3 d-flex justify-content-start">
-                                    <Button className="mx-3" variant="danger" name="Limpar"
-                                        onClick={() => apagar('cart')
-                                        }>
-                                    </Button>
-                                </div>
-                                {/* Fim Div Lixeira */}
-                            </div>
-
-
-
-                        </div>
-                        {/* Fim Container Lado direito    */}
+                    <div className="col-8 col-sm-8 col-md-8 col-lg-9 col-xl-9">
+                        <p className="text-center m-0 price-style">
+                            {priceConverted(cart[7])}
+                        </p>
+                    </div>
+                    {/* Inicio Lixeira */}
+                    <div className="col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2 d-flex justify-content-start ">
+                        <Button className="" name='x'
+                            onClick={() => {
+                                deleteCart('cart')
+                                getCart()
+                            }
+                            }>
+                        </Button>
                     </div>
 
-
-                </main>
-                {/* END CONTAINER PRINCIPAL */}
-
-
-                < Footer />
-            </>
-
+                </div>
+            </div>
         )
     }
 
-    export default Cart
+    //INÍCIO: TRANSFORMAÇÃO PARA SEPARAÇÃO DE DEZENAS E MILHARES COM PADRÃO BRASILEIRO
+    const priceConverted = (number) => {
+        return new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(number)
+    }
+
+    return (
+        <>
+
+            <Header />
+
+            <MiniCart />
+
+            {/* BEGIN TITULO PAGINA */}
+
+            {/* BEGIN CONTAINER PRINCIPAL */}
+            <main className="background-grey mt-2 mb-0">
+
+                <div className="row col-12 col-sm-12 col-md-12 col-lg-11 col-xl-11 m-auto">
+                    {/* Início Container Lado esquerdo */}
+                    <div className="col-12 col-sm-12 col-md-3 col-lg-3 col-xl-3">
+                        {/* Inicio subtitulo lado esquerdo */}
+                        <div className="">
+                            <Subtittle menu="Menu" />
+                        </div>
+                        {/* Fim subtitulo lado esquerdo */}
+
+                        {/* Inicio Sidebar */}
+                        <div className="">
+                            <Sidebar />
+                        </div>
+                        {/* Fim Sidebar */}
+                    </div>
+                    {/* Fim Container Lado esquerdo */}
+
+
+                    {/* Início Container Lado direito */}
+                    <div className="col-12 col-sm-12 col-md-9 col-lg-9 col-xl-9 px-0">
+
+                        {/* Inicio subtitulo lado direito */}
+                        <div className="">
+                            <Subtittle menu="Carrinho de compra" />
+                        </div>
+                        {/* Fim subtitulo lado direito */}
+
+
+
+
+
+                        {
+                            localStorage.getItem('cart')
+                                ? <>
+                                    {CardProduct()}
+                                    <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 px-0 mt-3 d-flex justify-content-center d-md-flex justify-content-md-center">
+                                        <div className="col-11 col-md-11 col-lg-12 col-xl-12 d-lg-flex justify-content-lg-end">
+                                            <Button link="/checkoutOrder" name="Continuar" />
+                                        </div>
+                                    </div>
+                                </>
+                                :
+                                <>
+                                    <div className="my-5 mx-5 d-flex justify-content-center">
+                                        <h1>Nao ha carros na Garagem </h1>
+                                    </div>
+                                    <div className="col-11 col-md-11 col-lg-12 col-xl-12 d-lg-flex justify-content-lg-end">
+                                        <Button link="/inventory" name="Estoque" />
+                                    </div>
+                                </>
+
+                        }
+
+
+
+                    </div>
+                    {/* Fim Container Lado direito    */}
+                </div>
+
+
+            </main>
+            {/* END CONTAINER PRINCIPAL */}
+
+
+            < Footer />
+        </>
+
+    )
+
+}
+
+export default Cart
