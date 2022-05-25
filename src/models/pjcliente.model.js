a4f709cf68de6281b859fea09767af296af6bf05
Guilherme <vianaguilherme29@gmail.com>
2022-05-25 15:51:09 -0300

    Cadastro PJ no banco realizado

--- src/models/pjcliente.model.js
+++ src/models/pjcliente.model.js
@@ -1,10 +1,10 @@
 const PJClient = {
-    razaoSocial: "Guilherme Corporation",
-    emailCliente: "gui@gmail.com",
-    numeroDocumento: "123456789",
-    inscricaoEstadual: "00012345678",
-    telefoneCliente: "99999999",
-    senhaCliente: "gui123"
+    razaoSocial: "",
+    emailCliente: "",
+    numeroDocumento: "",
+    inscricaoEstadual: "",
+    telefoneCliente: "",
+    senhaCliente: ""
 }
 
 export default PJClient
