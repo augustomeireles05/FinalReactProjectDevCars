8cc3ac01928676d6ede9ce48ba96a6956dc344a7
Augusto Meireles <augustomeireles05@gmail.com>
2022-05-26 19:35:07 -0300

    Add

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -5,13 +5,7 @@ function Input(props) {
 
     return (
         <>
-<<<<<<< HEAD
             <input className="input-style col-12 p-3 fs-6" type={props.type} aria-label={props.ariaLabel} id={props.id} valueautocomplete="off" value={props.value} onChange={props.onChange}/>
-=======
-
-            <input className="input-style col-12 p-3 fs-6" type={props.type} aria-label={props.ariaLabel} id={props.id} valueautocomplete="off" value={props.value} onChange={props.onChange}/>
-
->>>>>>> 6e1e23e63f2cdcf8dcf69ffc7d8ca71b3ccaac9f
         </>
     )
 }
