f998a57a43e844d06f67e539cf9eec27859c8cd5
LarissaBSouza <larissabandeirasouza@gmail.com>
2022-05-27 22:28:38 -0300

    Conflitos Resolvidos

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -40,7 +40,7 @@ function Register() {
 
             }).catch((err) => {
                 console.error("ops! ocorreu um erro" + err);
-              });
+            });
 
         history.push("/login")
         console.log("registrado")
@@ -48,23 +48,23 @@ function Register() {
 
     }
 
-   //INICIO DE TRATAMENTO DE PESSOA JURÍDICA
+    //INICIO DE TRATAMENTO DE PESSOA JURÍDICA
     const URLPJ = `${baseUrl}/cliente/j`
     const [registerPJ, setRegisterPJ] = useState(PJClient)
 
     const [confirmPasswordPJ, setConfirmPasswordPJ] = useState('')
 
     const registerPJCliente = () => {
-        axios.post(`${URLPJ}`, registerPJ )
+        axios.post(`${URLPJ}`, registerPJ)
             .then((response) => {
-                                      
-                
+
+
             })
 
-            history.push("/login")
-            
+        history.push("/login")
 
-    } 
+
+    }
 
     const comparePasswordsPJ = () => {
         if (registerPJ.senhaCliente != confirmPasswordPJ) {
@@ -119,7 +119,7 @@ function Register() {
                             </div>
                             <div className="col-12 col-md-12 col-lg-6 mb-3">
                                 <Label label="CPF" htmlFor="cpf" />
-                                <Input type="text" aria-label="cpf" id="cpf" value={registerPF.numeroDocumento}
+                                <Input type="text" aria-label="cpf" id="cpf" maxlength="14" value={registerPF.numeroDocumento}
                                     onChange={(event) => { setRegisterPF({ ...registerPF, numeroDocumento: event.target.value }) }} />
                             </div>
                         </div>
@@ -127,12 +127,12 @@ function Register() {
                         <div className="row">
                             <div className="col-12 col-md-12 col-lg-6 mb-5">
                                 <Label label="Data de Nascimento" htmlFor="dtNascimento" />
-                                <Input type="text" aria-label="dtNascimento" id="dtNascimento" value={registerPF.dataNascimento}
+                                <Input type="text" aria-label="dtNascimento" maxlength="8" id="dtNascimento" value={registerPF.dataNascimento}
                                     onChange={(event) => { setRegisterPF({ ...registerPF, dataNascimento: event.target.value }) }} />
                             </div>
                             <div className="col-12 col-md-12 col-lg-6 mb-3">
                                 <Label label="Telefone" htmlFor="telefonePF" />
-                                <Input type="text" aria-label="telefonePF" id="telefonePF" value={registerPF.telefoneCliente}
+                                <Input type="text" aria-label="telefonePF" maxlength="11" id="telefonePF" value={registerPF.telefoneCliente}
                                     onChange={(event) => { setRegisterPF({ ...registerPF, telefoneCliente: event.target.value }) }} />
                             </div>
                         </div>
@@ -170,19 +170,19 @@ function Register() {
 
                                     setConfirmPassword(event.target.value)
 
-                                    if(event.target.value === registerPF.senhaCliente ){
-                                    
-                                        setStatus({type:'sucess', mensagem:'ok'})
-                                    } else{
-                                    
-                                        setStatus({type:'error', mensagem:'senhas divergentes'})
+                                    if (event.target.value === registerPF.senhaCliente) {
+
+                                        setStatus({ type: 'sucess', mensagem: 'ok' })
+                                    } else {
+
+                                        setStatus({ type: 'error', mensagem: 'senhas divergentes' })
                                     }
                                 }} />
 
                             {
                                 status.type === 'sucess'
                                     ?
-                                    <span style={{ color: 'white', backgroundColor:'green', padding:'2px 15px'}}>
+                                    <span style={{ color: 'white', backgroundColor: 'green', padding: '2px 15px' }}>
                                         {status.mensagem}
                                     </span>
                                     :
@@ -194,7 +194,7 @@ function Register() {
                             {
                                 status.type === 'error'
                                     ?
-                                    <span style={{ color: 'white', backgroundColor:'red', padding:'2px 15px'}}>
+                                    <span style={{ color: 'white', backgroundColor: 'red', padding: '2px 15px' }}>
                                         {status.mensagem}
                                     </span>
                                     :
@@ -240,19 +240,19 @@ function Register() {
                         <div className="col-md-12 mb-3">
                             <Label label="Razão Social" htmlFor="rzSocial" />
                             <Input type="text" aria-label="rzSocial" id="rzSocial" value={registerPJ.razaoSocial}
-                           onChange={(event) => { setRegisterPJ({ ...registerPJ, razaoSocial: event.target.value }) }} />
+                                onChange={(event) => { setRegisterPJ({ ...registerPJ, razaoSocial: event.target.value }) }} />
                         </div>
 
                         <div className="row">
                             <div className="col-12 col-md-12 col-lg-6 mb-3">
                                 <Label label="Email" htmlFor="emailPJ" />
-                                <Input type="email" aria-label="emailPJ" id="emailPJ" value={registerPJ.emailCliente} 
-                                onChange={(event) => { setRegisterPJ({ ...registerPJ, emailCliente: event.target.value }) }} />
+                                <Input type="email" aria-label="emailPJ" id="emailPJ" value={registerPJ.emailCliente}
+                                    onChange={(event) => { setRegisterPJ({ ...registerPJ, emailCliente: event.target.value }) }} />
                             </div>
                             <div className="col-12 col-md-12 col-lg-6 mb-3">
                                 <Label label="CNPJ" htmlFor="cnpj" />
-                                <Input type="text" aria-label="cnpj" id="cnpj" value={registerPJ.numeroDocumento} 
-                                onChange={(event) => { setRegisterPJ({ ...registerPJ, numeroDocumento: event.target.value }) }} />
+                                <Input type="text" aria-label="cnpj" maxlength="14" id="cnpj" value={registerPJ.numeroDocumento}
+                                    onChange={(event) => { setRegisterPJ({ ...registerPJ, numeroDocumento: event.target.value }) }} />
                             </div>
                         </div>
 
@@ -261,14 +261,14 @@ function Register() {
                         <div className="row">
                             <div className="col-12 col-md-12 col-lg-6 mb-5">
                                 <Label label="Inscrição Estadual" htmlFor="inscrEstadual" />
-                                <Input type="text" aria-label="inscrEstadual" id="inscrEstadual" value={registerPJ.inscricaoEstadual}
-                                onChange={(event) => { setRegisterPJ({ ...registerPJ, inscricaoEstadual: event.target.value }) }} />
-                                
+                                <Input type="text" aria-label="inscrEstadual" maxlength="11" id="inscrEstadual" value={registerPJ.inscricaoEstadual}
+                                    onChange={(event) => { setRegisterPJ({ ...registerPJ, inscricaoEstadual: event.target.value }) }} />
+
                             </div>
                             <div className="col-12 col-md-12 col-lg-6 mb-5">
                                 <Label label="Telefone" htmlFor="telefonePJ" />
-                                <Input type="text" aria-label="telefonePJ" id="telefonePJ" value={registerPJ.telefoneCliente}
-                                onChange={(event) => { setRegisterPJ({ ...registerPJ, telefoneCliente: event.target.value }) }}  />
+                                <Input type="text" aria-label="telefonePJ" maxlength="11" id="telefonePJ" value={registerPJ.telefoneCliente}
+                                    onChange={(event) => { setRegisterPJ({ ...registerPJ, telefoneCliente: event.target.value }) }} />
                             </div>
                         </div>
 
@@ -291,50 +291,50 @@ function Register() {
 
                         <div className="col-12 col-md-12 col-lg-6 mb-3">
                             <Label label="Escolha um senha" htmlFor="password-registerPJ" />
-                            <Input type="password" aria-label="password-registerPJ" id="password-registerPJ" 
+                            <Input type="password" aria-label="password-registerPJ" id="password-registerPJ"
                                 value={registerPJ.senhaCliente}
-                            onChange={(event) => { setRegisterPJ({ ...registerPJ, senhaCliente: event.target.value }) }} />
+                                onChange={(event) => { setRegisterPJ({ ...registerPJ, senhaCliente: event.target.value }) }} />
 
                         </div>
 
                         <div className="col-12 col-md-12 col-lg-6 mb-3">
                             <Label label="Confirme sua nova senha" htmlFor="password-register-ConfirmPasswordPJ" />
-                            <Input type="password" aria-label="password-register-confirmPasswordPJ" id="password-register-confirmPJ" 
-                                value={confirmPasswordPJ}                                
+                            <Input type="password" aria-label="password-register-confirmPasswordPJ" id="password-register-confirmPJ"
+                                value={confirmPasswordPJ}
                                 onChange={(event) => {
 
                                     setConfirmPasswordPJ(event.target.value)
-                                    if(event.target.value === registerPJ.senhaCliente){
+                                    if (event.target.value === registerPJ.senhaCliente) {
 
-                                         setStatus({type:'sucess', mensagem:'ok'})
-                                    } else{
+                                        setStatus({ type: 'sucess', mensagem: 'ok' })
+                                    } else {
 
-                                        setStatus({type:'error', mensagem:'senhas divergentes'})
-                                    }                           
+                                        setStatus({ type: 'error', mensagem: 'senhas divergentes' })
+                                    }
                                 }} />
                             {
                                 status.type === 'sucess'
-                                ?
-                                <span style={{ color: 'white', backgroundColor:'green', padding: '2px 15px'}}>
-                                    {status.mensagem}
-                                </span>
-                                :
-                                ""
-                            }   
+                                    ?
+                                    <span style={{ color: 'white', backgroundColor: 'green', padding: '2px 15px' }}>
+                                        {status.mensagem}
+                                    </span>
+                                    :
+                                    ""
+                            }
 
                             {
                                 status.type === 'error'
-                                ?
-                                <span style={{color: 'white', backgroundColor: 'red', padding: '2px 15px'}}>
-                                     {status.mensagem}   
-                                </span>
-                                :
-                                ""
-                            } 
+                                    ?
+                                    <span style={{ color: 'white', backgroundColor: 'red', padding: '2px 15px' }}>
+                                        {status.mensagem}
+                                    </span>
+                                    :
+                                    ""
+                            }
 
 
 
-                                
+
                         </div>
 
                         <div className="col-12 mt-4">
@@ -347,8 +347,8 @@ function Register() {
                         </div>
                         <div className="row justify-content-center justify-content-lg-start">
                             <div className="col-12 col-md-6 col-lg-3 text-center mt-4 mb-3 ">
-                            <Button name="CADASTRE-SE" onClick={registerPJCliente}/>
-                        </div>
+                                <Button name="CADASTRE-SE" onClick={registerPJCliente} />
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -356,6 +356,7 @@ function Register() {
             </div>
 
             <Footer />
+
         </>
     )
 }
